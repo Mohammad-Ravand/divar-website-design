33e9bde01be4a57dd1f101808886a907ade6ace1
fishopping marketplace <fishoppingmarket@gmail.com>
2023-03-24 17:00:56 +0430

    generate vite project with vite-extra and react ssr typescript

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Logo from './logo/Logo'
+
+type Props = {}
+
+const Header = (props: Props) => {
+  return (
+    <div>
+        {/* logo website */}
+        <Logo />
+
+        {/* set location with modal */}
+        
+
+        {/* select category  */}
+
+        {/* search box in advertises */}
+
+        {/* profile */}
+        {/* chats */}
+        {/* support website */}
+        {/* register advertise */}
+    </div>
+  )
+}
+
+export default Header
